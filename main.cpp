521d90bf0d3932daf3e303437624ca6c92bf432d
Jean Marc Nadeau Huamani <101827583+Jean-Marc-NH@users.noreply.github.com>
2024-03-18 19:00:55 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,24 +8,47 @@ int main() {
 
 	int A[20];
 
-	for (int i = 0; i < 20; i++) {
-		A[i] = i * 2;
+	for (int i = 0; i < 20; i++) { 
+		A[i] = (i + 1) * 2; 
 	}
 
 	int* ini = A;
-	int* fin = &A[20];
+	int* fin = &A[19];
 	int* pos = NULL;
 
-	if (BinBus(ini, fin, pos, 7))cout << "Encontrado\n";
+	for (int i = 0; i < 20; i++) {
+		cout << A[i] << " \n";
+	}
+
+	if (BinBus(ini, fin, pos, 20)) {
+		cout << "Encontrado\n";
+	}
+	else {
+		cout << "No encontrado";
+	}
 
 	return EXIT_SUCCESS;
 }
 
-bool BinBus(int* ini, int * fin, int* &pos, int key) {
-	while (true) {
-		if (ini > fin)return false;
+bool BinBus(int* ini, int* fin, int*& pos, int key) {
+	pos = ini;
+	while (ini <= fin) {
 
+		pos = pos + ((fin - ini)/2);
 
+		cout << *pos << " ";
+
+		if (*pos < key) {
+			ini = pos + 1;
+		}
+		else if(*pos > key) {
+			fin = pos - 1;
+			cout << "fin: " << *fin << endl;
+		}
+		else {
+			cout << "localizado\n";
+			return true;
+		}
 	}
-	return true;
+	return false;
 }
